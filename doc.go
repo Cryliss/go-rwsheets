959b72fe8d1aa3dd49352a18ccf2bd59b000d295
Sabra Bilodeau <sabrabilodeau@gmail.com>
2024-02-03 13:18:30 -0800

    Update doc.go

--- doc.go
+++ doc.go
@@ -1,6 +1,6 @@
 /*
 Package rwsheets (rwsheets.go) :
-This is a Golang library to retrieve Google Sheet data and update Google sheet data, with additional helper functions for updating the sheet values.
+This is a Golang library to retrieve and update Google Sheet data, with additional helper functions for updating the cell values.
 # Install
 $ go get -u github.com/cryliss/go-rwsheets
 More information is https://github.com/cryliss/go-rwsheets
